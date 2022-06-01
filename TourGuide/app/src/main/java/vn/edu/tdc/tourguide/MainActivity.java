d737356b7736d744d00c3b3b7a52a97a5e4a5e1f
0965655783 <ducnm.90.student@fit.tdc.edu.vn>
2022-06-01 10:59:52 +0700

    Connect Firebase

--- TourGuide/app/src/main/java/vn/edu/tdc/tourguide/MainActivity.java
+++ TourGuide/app/src/main/java/vn/edu/tdc/tourguide/MainActivity.java
@@ -1,8 +1,16 @@
 package vn.edu.tdc.tourguide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("MinhDuc", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("MinhDuc", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
