0915f6f2ca0f8df5ecef105f67d5ba2bb2c8c421
0965655783 <ducnm.90.student@fit.tdc.edu.vn>
2022-06-03 10:39:20 +0700

    CRUD (Display image)

--- TourGuide/app/src/main/java/vn/edu/tdc/tourguide/MainActivity.java
+++ TourGuide/app/src/main/java/vn/edu/tdc/tourguide/MainActivity.java
@@ -18,20 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("MinhDuc", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w("MinhDuc", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
+
+        //        CityModel.getCities();
+//        CityModel.addCity("HCM", "boy.png");
+//        CityModel.updateCity("1","Ha Noi", "boy.png");
+//        CityModel.deleteCity("1");
+
+//        DestinationModel.getDestination();
+//        DestinationModel.addDestination("1", "Hoành Thành Thăng Long", "Du lịch","boy.png", 325245, 324234, "19C Hoàng Diệu, Điện Bàn, Ba Đình, Hà Nội", 5);
+//        DestinationModel.updateDestination("2", "1", "Lăng Bác và Quảng Trường Ba Đình", "Chợ","boy.png", 3253245, 324234, "2 Hùng Vương, Điện Bà, Ba Đình, Hà Nội", 5);
+//        DestinationModel.deleteDestination("2");
+
+//        ReviewModel.getReview();
+//        ReviewModel.addReview("1", "duc", "nguyenminhduc.tdc2020@gmail.com", "Tour rat tuyet", 5);
+//        ReviewModel.updateReview("3", "1", "minh", "nguyenminhduc.tdc2020@gmail.com", "Tour tổ chức tốt", 5);
+//        ReviewModel.deleteReview("3");
+
+//        ScheduleModel.getSchedule();
+//        ScheduleModel.addSchedule("nguyenminhduc.tdc2020@gmail.com", "12/04/2021", "du lịch", "2");
+//        ScheduleModel.updateSchedule("5","nguyenminhduc.tdc2020@gmail.com", "12/04/2022", "Đi biển", "2");
+//        ScheduleModel.deleteSchedule("5");
     }
 }
